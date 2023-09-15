1da1b257b6ee3c47c66e3e1b5cee247cd2ac5471
angelique <angelicrf@gmail.com>
2023-09-15 11:17:23 -0700

    img-added

--- components/r-image.js
+++ components/r-image.js
@@ -1,13 +1,13 @@
 const RImage = () => {
   return (
-    <div className="my-0 mx-[!important] absolute w-[calc(100%_-_204px)] top-[142px] right-[102px] left-[102px] flex flex-col items-start justify-start z-[0] md:w-auto md:[align-self:unset] md:h-auto sm:self-stretch sm:w-auto sm:flex-1">
+    <div className='my-0 mx-[!important] absolute w-[calc(100%_-_204px)] top-[142px] right-[102px] left-[102px] flex flex-col items-start justify-start z-[0] md:w-auto md:[align-self:unset] md:h-auto sm:self-stretch sm:w-auto sm:flex-1'>
       <img
-        className="relative w-[295px] h-[609px] object-cover sm:self-stretch sm:w-auto"
-        alt=""
-        src="/rimageshape@2x.png"
+        className='relative w-[295px] h-[609px] object-cover sm:self-stretch sm:w-auto'
+        alt=''
+        src='rimageshape@2x.png'
       />
     </div>
-  );
-};
+  )
+}
 
-export default RImage;
+export default RImage
