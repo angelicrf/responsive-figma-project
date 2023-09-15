7a6ddcc62f499c860267c1ccb163d0f24f6ad917
angelique <angelicrf@gmail.com>
2023-09-15 10:09:08 -0700

    links-updates

--- components/frame-component.js
+++ components/frame-component.js
@@ -1,25 +1,40 @@
 const FrameComponent = () => {
   return (
-    <div className="self-stretch h-[679px] flex flex-col items-start justify-between text-center text-5xl text-black font-inter lg:self-stretch lg:w-auto lg:h-auto lg:gap-[72px] md:self-stretch md:w-auto md:flex-col md:gap-[20px] sm:self-stretch sm:w-auto sm:h-auto sm:gap-[0px]">
-      <div className="self-stretch flex flex-row p-5 items-start justify-between md:self-stretch md:w-auto md:h-auto md:flex-row md:gap-[20px] sm:self-stretch sm:w-auto sm:h-auto sm:flex-row sm:gap-[0px]">
-        <b className="relative sm:font-inter sm:text-mid">link one</b>
-        <b className="relative sm:font-inter sm:text-mid">link Two</b>
-        <b className="relative sm:font-inter sm:text-mid">link Three</b>
+    <div className='self-stretch h-[679px] flex flex-col items-start justify-between text-center text-5xl text-black font-inter lg:self-stretch lg:w-auto lg:h-auto lg:gap-[72px] md:self-stretch md:w-auto md:flex-col md:gap-[20px] sm:self-stretch sm:w-auto sm:h-auto sm:gap-[0px]'>
+      <div className='self-stretch flex flex-row p-5 items-start justify-between md:self-stretch md:w-auto md:h-auto md:flex-row md:gap-[20px] sm:self-stretch sm:w-auto sm:h-auto sm:flex-row sm:gap-[0px]'>
+        <b
+          className='relative cursor-pointer sm:font-inter sm:text-mid'
+          onClick={() => alert('linkOne')}
+        >
+          link one
+        </b>
+        <b
+          className='relative cursor-pointer sm:font-inter sm:text-mid'
+          onClick={() => alert('linkTWo')}
+        >
+          link Two
+        </b>
+        <b
+          className='relative cursor-pointer sm:font-inter sm:text-mid'
+          onClick={() => alert('linkThree')}
+        >
+          link Three
+        </b>
       </div>
-      <div className="w-[631px] h-[538px] flex flex-col items-start justify-between lg:self-stretch lg:w-auto md:self-stretch md:w-auto md:flex-col md:gap-[20px] sm:self-stretch sm:w-auto sm:h-auto">
+      <div className='w-[631px] h-[538px] flex flex-col items-start justify-between lg:self-stretch lg:w-auto md:self-stretch md:w-auto md:flex-col md:gap-[20px] sm:self-stretch sm:w-auto sm:h-auto'>
         <img
-          className="self-stretch relative max-w-full overflow-hidden h-[235px] shrink-0 object-cover lg:self-stretch lg:w-auto md:self-stretch md:w-auto md:flex-1 md:h-auto sm:self-stretch sm:w-auto sm:h-auto"
-          alt=""
-          src="/rectangle-1@2x.png"
+          className='self-stretch relative max-w-full overflow-hidden h-[235px] shrink-0 object-cover lg:self-stretch lg:w-auto md:self-stretch md:w-auto md:flex-1 md:h-auto sm:self-stretch sm:w-auto sm:h-auto'
+          alt=''
+          src='/rectangle-1@2x.png'
         />
         <img
-          className="self-stretch relative max-w-full overflow-hidden h-[235px] shrink-0 object-cover lg:self-stretch lg:w-auto lg:flex-1 md:self-stretch md:w-auto md:flex-1 md:h-auto sm:self-stretch sm:w-auto sm:h-auto"
-          alt=""
-          src="/rectangle-11@2x.png"
+          className='self-stretch relative max-w-full overflow-hidden h-[235px] shrink-0 object-cover lg:self-stretch lg:w-auto lg:flex-1 md:self-stretch md:w-auto md:flex-1 md:h-auto sm:self-stretch sm:w-auto sm:h-auto'
+          alt=''
+          src='/rectangle-11@2x.png'
         />
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default FrameComponent;
+export default FrameComponent
--- components/left-links.js
+++ components/left-links.js
@@ -1,58 +1,73 @@
 const LeftLinks = () => {
   return (
-    <div className="self-stretch h-[652px] flex flex-col items-center justify-between text-center text-5xl text-black font-inter lg:w-auto lg:[align-self:unset] lg:h-auto lg:gap-[60px] lg:pt-5 lg:box-border md:w-auto md:[align-self:unset] md:h-auto md:flex-col md:pt-[100px] md:box-border sm:flex-1 sm:gap-[50px] sm:items-center sm:justify-center sm:pt-5 sm:pb-5 sm:box-border">
-      <div className="flex flex-row items-start justify-start gap-[40px] md:w-auto md:[align-self:unset] sm:self-stretch sm:w-auto sm:flex-1 sm:gap-[40px]">
+    <div className='self-stretch h-[652px] flex flex-col items-center justify-between text-center text-5xl text-black font-inter lg:w-auto lg:[align-self:unset] lg:h-auto lg:gap-[60px] lg:pt-5 lg:box-border md:w-auto md:[align-self:unset] md:h-auto md:flex-col md:pt-[100px] md:box-border sm:flex-1 sm:gap-[50px] sm:items-center sm:justify-center sm:pt-5 sm:pb-5 sm:box-border'>
+      <div className='flex flex-row items-start justify-start gap-[40px] md:w-auto md:[align-self:unset] sm:self-stretch sm:w-auto sm:flex-1 sm:gap-[40px]'>
         <img
-          className="self-stretch relative max-h-full w-[30px] sm:w-[25px] sm:h-[25px]"
-          alt=""
-          src="/vector.svg"
+          className='self-stretch relative max-h-full w-[30px] sm:w-[25px] sm:h-[25px]'
+          alt=''
+          src='/vector.svg'
         />
-        <b className="relative lg:flex-1 md:flex-1 sm:text-lg sm:flex-1 sm:self-stretch sm:h-auto">
+        <b
+          className='relative cursor-pointer lg:flex-1 md:flex-1 sm:text-lg sm:flex-1 sm:self-stretch sm:h-auto'
+          onClick={() => alert('testOne')}
+        >
           TestOne
         </b>
       </div>
-      <div className="flex flex-row items-start justify-start gap-[40px] md:w-auto md:[align-self:unset] sm:self-stretch sm:w-auto sm:flex-1 sm:gap-[40px]">
+      <div className='flex flex-row items-start justify-start gap-[40px] md:w-auto md:[align-self:unset] sm:self-stretch sm:w-auto sm:flex-1 sm:gap-[40px]'>
         <img
-          className="self-stretch relative max-h-full w-[30px] sm:w-[25px] sm:h-[25px]"
-          alt=""
-          src="/vector1.svg"
+          className='self-stretch relative max-h-full w-[30px] sm:w-[25px] sm:h-[25px]'
+          alt=''
+          src='/vector1.svg'
         />
-        <b className="relative lg:flex-1 md:flex-1 sm:text-lg sm:flex-1 sm:self-stretch sm:h-auto">
+        <b
+          className='relative cursor-pointer lg:flex-1 md:flex-1 sm:text-lg sm:flex-1 sm:self-stretch sm:h-auto'
+          onClick={() => alert('testTwo')}
+        >
           TestTwo
         </b>
       </div>
-      <div className="flex flex-row items-start justify-start gap-[40px] md:w-auto md:[align-self:unset] sm:self-stretch sm:w-auto sm:flex-1 sm:gap-[40px]">
+      <div className='flex flex-row items-start justify-start gap-[40px] md:w-auto md:[align-self:unset] sm:self-stretch sm:w-auto sm:flex-1 sm:gap-[40px]'>
         <img
-          className="self-stretch relative max-h-full w-[30px] sm:w-[25px] sm:h-[25px]"
-          alt=""
-          src="/vector2.svg"
+          className='self-stretch relative max-h-full w-[30px] sm:w-[25px] sm:h-[25px]'
+          alt=''
+          src='/vector2.svg'
         />
-        <b className="relative lg:flex-1 md:flex-1 sm:text-lg sm:flex-1 sm:self-stretch sm:h-auto">
+        <b
+          className='relative cursor-pointer lg:flex-1 md:flex-1 sm:text-lg sm:flex-1 sm:self-stretch sm:h-auto'
+          onClick={() => alert('testThree')}
+        >
           TestThree
         </b>
       </div>
-      <div className="flex flex-row items-start justify-start gap-[40px] md:w-auto md:[align-self:unset] sm:self-stretch sm:w-auto sm:flex-1 sm:gap-[40px]">
+      <div className='flex flex-row items-start justify-start gap-[40px] md:w-auto md:[align-self:unset] sm:self-stretch sm:w-auto sm:flex-1 sm:gap-[40px]'>
         <img
-          className="self-stretch relative max-h-full w-[30px] sm:w-[25px] sm:h-[25px]"
-          alt=""
-          src="/vector3.svg"
+          className='self-stretch relative max-h-full w-[30px] sm:w-[25px] sm:h-[25px]'
+          alt=''
+          src='/vector3.svg'
         />
-        <b className="relative lg:flex-1 md:flex-1 sm:text-lg sm:flex-1 sm:self-stretch sm:h-auto">
+        <b
+          className='relative cursor-pointer lg:flex-1 md:flex-1 sm:text-lg sm:flex-1 sm:self-stretch sm:h-auto'
+          onClick={() => alert('testFour')}
+        >
           TestFour
         </b>
       </div>
-      <div className="flex flex-row items-start justify-start gap-[40px] md:w-auto md:[align-self:unset] sm:self-stretch sm:w-auto sm:flex-1 sm:gap-[40px]">
+      <div className='flex flex-row items-start justify-start gap-[40px] md:w-auto md:[align-self:unset] sm:self-stretch sm:w-auto sm:flex-1 sm:gap-[40px]'>
         <img
-          className="self-stretch relative max-h-full w-[30px] sm:w-[25px] sm:h-[25px]"
-          alt=""
-          src="/vector4.svg"
+          className='self-stretch relative max-h-full w-[30px] sm:w-[25px] sm:h-[25px]'
+          alt=''
+          src='/vector4.svg'
         />
-        <b className="relative lg:flex-1 md:flex-1 sm:text-lg sm:flex-1 sm:self-stretch sm:h-auto">
+        <b
+          className='relative cursor-pointer lg:flex-1 md:flex-1 sm:text-lg sm:flex-1 sm:self-stretch sm:h-auto'
+          onClick={() => alert('testFive')}
+        >
           TestFive
         </b>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default LeftLinks;
+export default LeftLinks
